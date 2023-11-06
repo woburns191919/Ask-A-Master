bc630e3f4e056700f7ee5502ed6bdc3215cd2ae6
will <woburns191919>
2023-11-06 13:36:47 -0600

    topic update

--- react-app/src/components/TopicQuestionsPage/index.js
+++ react-app/src/components/TopicQuestionsPage/index.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import "./styles.css";
+import "./styles.css"; // Import the same stylesheet
 
-
-import middleGameImage from '../../images/images.png';
-import fischer from '../../images/fischer.png';
-import endGame from '../../images/endGame.png';
-import platforms from '../../images/platforms.jpg';
-import analysis from '../../images/analysis.png';
-import structure from '../../images/structure.jpg';
-import blunders from '../../images/blunder.png'
+import middleGameImage from "../../images/images.png";
+import fischer from "../../images/fischer.png";
+import endGame from "../../images/endGame.png";
+import platforms from "../../images/platforms.jpg";
+import analysis from "../../images/analysis.png";
+import structure from "../../images/structure.jpg";
+import blunders from "../../images/blunder.png";
 
 const TopicQuestionsPage = () => {
   const { id } = useParams();
   const [questions, setQuestions] = useState([]);
   const [allTopics, setAllTopics] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState(null);
 
   const fetchQuestionsByTopic = async () => {
     try {
@@ -24,7 +24,10 @@ const TopicQuestionsPage = () => {
         const data = await res.json();
         setQuestions(data.questions);
       } else {
-        console.error("Failed to fetch questions for the topic. Status:", res.status);
+        console.error(
+          "Failed to fetch questions for the topic. Status:",
+          res.status
+        );
       }
     } catch (error) {
       console.error("Failed to fetch questions for the topic:", error);
@@ -37,6 +40,8 @@ const TopicQuestionsPage = () => {
       if (res.ok) {
         const data = await res.json();
         setAllTopics(data.topics);
+        const topic = data.topics.find((topic) => topic.id === parseInt(id));
+        setSelectedTopic(topic);
       } else {
         console.error("Failed to fetch topics. Status:", res.status);
       }
@@ -51,84 +56,80 @@ const TopicQuestionsPage = () => {
   }, [id]);
 
   return (
-    <div className="topic-questions-page">
-      <div className="topic-side-panel">
-        <h2>All Topics</h2>
-        <ul>
-          {allTopics.map((topic) => (
-            <li key={topic.id}>
-              <Link to={`/topics/${topic.id}`}>{topic.name}</Link>
-            </li>
-          ))}
-        </ul>
+    <main className="main-container">
+      <div className="topic-question-container">
+        <div className="question-answer-box">
+          <div className="question-box">
+            <h2>All Topics</h2>
+            <ul className="topic-list">
+              {" "}
+              {/* Adding a new class for the topics */}
+              {allTopics.map((topic) => (
+                <li key={topic.id}>
+                  <Link to={`/topics/${topic.id}`}>{topic.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="answer-box">
+            <h3>{selectedTopic ? selectedTopic.name : ""}</h3>
+            <ul>
+              {questions.map((question, index) => (
+                <li key={index}>
+                  <h4>{question.body}</h4>
+                  <ul>
+                    {question.answers?.map((answer, i) => (
+                      <li key={i}>{answer.content}</li>
+                    ))}
+                  </ul>
+                  {question.body.includes("Defense") && (
+                    <img
+                      src={middleGameImage}
+                      alt="Middle Game Photo"
+                      className="photos"
+                    />
+                  )}
+                  {question.body.includes("books") && (
+                    <img src={fischer} alt="Fischer Photo" className="photos" />
+                  )}
+                  {question.body.includes("endgame") && (
+                    <img src={endGame} alt="Endgame Photo" className="photos" />
+                  )}
+                  {question.body.includes("platforms") && (
+                    <img
+                      src={platforms}
+                      alt="Platforms Photo"
+                      className="photos"
+                    />
+                  )}
+                  {question.body.includes("analysis") && (
+                    <img
+                      src={analysis}
+                      alt="Analysis Photo"
+                      className="photos"
+                    />
+                  )}
+                  {question.body.includes("blunders") && (
+                    <img
+                      src={blunders}
+                      alt="Blunders Photo"
+                      className="photos"
+                    />
+                  )}
+                  {question.body.includes("structure") && (
+                    <img
+                      src={structure}
+                      alt="Structure Photo"
+                      className="photos"
+                    />
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="topic-questions-content">
-        <h1>Questions related to the topic with ID {id}</h1>
-        <ul>
-          {questions.map((question) => (
-            <li key={question.id}>
-              <h3>{question.body}</h3>
-              <ul>
-                {question.answers?.map((answer) => (
-                  <li key={answer.id}>{answer.content}</li>
-                ))}
-              </ul>
-
-              {question.body.includes("Defense") && (
-                <img
-                  src={middleGameImage}
-                  alt="Middle Game Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("books") && (
-                <img
-                  src={fischer}
-                  alt="Fischer Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("endgame") && (
-                <img
-                  src={endGame}
-                  alt="Endgame Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("platforms") && (
-                <img
-                  src={platforms}
-                  alt="Platforms Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("analysis") && (
-                <img
-                  src={analysis}
-                  alt="Analysis Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("blunders") && (
-                <img
-                  src={blunders}
-                  alt="Blunders Photo"
-                  className="question-image"
-                />
-              )}
-              {question.body.includes("structure") && (
-                <img
-                  src={structure}
-                  alt="Structure Photo"
-                  className="question-image"
-                />
-              )}
-
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </main>
   );
 };
 
