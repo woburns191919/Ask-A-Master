44334563a9fc777268058cdf704bf5d5c9b59d73
will <woburns191919>
2023-11-07 11:44:42 -0600

    posts and routes parent dir

--- app/api/questions_routes.py
+++ app/api/questions_routes.py
@@ -1,13 +1,10 @@
-from flask import Blueprint, jsonify, request,redirect, url_for, abort
+from flask import Blueprint, jsonify, request, redirect, url_for, abort
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Question, db
 from app.models.answer import Answer
-from sqlalchemy import func, distinct, or_, desc
-# from ..forms import
-
+from app.forms import question_form
 
 questions_routes = Blueprint('questions', __name__)
-
 
 @questions_routes.route("<int:question_id>/answers")
 def get_answers_for_question(question_id):
@@ -21,3 +18,36 @@ def get_all_questions():
   questions = db.session.query(Question).all()
   all_questions = {'questions': [question.to_dict() for question in questions]}
   return jsonify(all_questions)
+
+from flask import Blueprint, request, jsonify
+from app.models import db, Question
+
+questions_routes = Blueprint('questions', __name__)
+
+
+@questions_routes.route('/new', methods=['POST'])
+def create_question():
+
+    data = request.get_json()
+
+    title = data.get('title')
+    body = data.get('body')
+    user_id = data.get('user_id')
+    topic_id = data.get('topic_id')
+
+
+    # if not title or not body or user_id is None or topic_id is None:
+    #     return jsonify({'error': 'Missing required fields'}), 400
+
+
+    new_question = Question(
+        title=title,
+        body=body,
+        user_id=user_id,
+        topic_id=topic_id
+    )
+
+    db.session.add(new_question)
+    db.session.commit()
+
+    return jsonify({'message': 'Question created successfully', 'question': new_question.to_dict()}), 201
