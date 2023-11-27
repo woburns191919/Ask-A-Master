6d98f9ff4453a4a1f76ecf5f76ba3bbe062e5526
will <woburns191919>
2023-11-27 13:41:06 -0600

    latest fix

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -34,6 +34,7 @@ function ProfileButton({ user }) {
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
+
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
